bb4b5ae107b2d7aca7c5d1eff1364caa91da7dad
tpe1ga <PedroIvan.TelloFlores@mx.bosch.com>
2022-11-14 19:39:08 -0600

    Testing case in thsirts size problem

--- tshirts.py
+++ tshirts.py
@@ -8,7 +8,19 @@ def size(cms):
         return 'L'
 
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
-assert(size(43) == 'L')
+# assert(size(37) == 'S')
+# assert(size(40) == 'M')
+# assert(size(43) == 'L')
+# 38 is out of his input because it maybe needs to clasify as S or M
+# inside if condition but currently its L in any case
+# This loop are created to include 38 inside M size using >= evaluation
+for x in range(100):
+    print(x)
+    if x < 38:
+        assert(size(x) == 'S')
+    elif x >= 38 and x < 42:
+        assert(size(x) == 'M')
+    else:
+        assert(size(x) == 'L')
+
 print("All is well (maybe!)\n")
