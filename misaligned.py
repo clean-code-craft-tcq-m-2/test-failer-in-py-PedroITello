abb856f2ff1652e5863ecf17dd3dbfd2d5557042
tpe1ga <PedroIvan.TelloFlores@mx.bosch.com>
2022-11-25 09:20:34 -0600

    Some modification on misaligned to use center function on string construction

--- misaligned.py
+++ misaligned.py
@@ -1,15 +1,15 @@
-major_colors = ["White ", "Red   ", "Black ", "Yellow", "Violet"] # Normalize strings for alignment
-minor_colors = ["Blue  ", "Orange", "Green ", "Brown ", "Slate "]
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"] # Normalize strings for alignment
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 def print_color_map():
     temp_list = []
+    space = 6
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
+            interval = i * 5 + j + 1
             # Save the output into a list
-            if ((i * 5 + j + 1) > 9):
-                temp_list.append(f'{i * 5 + j + 1} | {major} | {minor}') # Add the i * 5 + j + 1
-            else:
-                temp_list.append(f'0{i * 5 + j + 1} | {major} | {minor}') # Add 0 at start of number for alignment
+            string = f'{interval}'.center(space)+'|'+f'{major}'.center(space)+'|'+f'{minor}'.center(space)+'|'
+            temp_list.append(string) # Add 0 at start of number for alignment
     # Print color map codes
     print(*temp_list, sep = "\n")
     # Add a return value called "temp_list" that cointains the output
@@ -18,7 +18,7 @@ def print_color_map():
 # Define new function to test pair color code
 def get_pair_number_from_color(major_color, minor_color):
     try:
-        major_index = major_colors.index(major_color[:-1])
+        major_index = major_colors.index(major_color)
     except ValueError:
         # raise Exception('Major index out of range')
         return False
@@ -34,13 +34,13 @@ result, output = print_color_map()
 #First evaluate the fullfield pair of colors
 assert(result == 25)
 for index, data in enumerate(output):
-    expected_index, major_color, minor_color = data.split('|')
+    expected_index, major_color, minor_color = data.split('|',2)
     # Check the first position in where are located the '|' separator inside the list
     if index == 0:
         first_separator_position = data.find('|')
     # Validate pair color string welformed
-    pair_number = get_pair_number_from_color(major_color.lstrip(), minor_color.lstrip())
-    assert(pair_number == int(expected_index))
+    pair_number = get_pair_number_from_color(major_color.strip(), minor_color.replace('|','').strip())
+    assert(pair_number == int(expected_index.strip()))
     # Check alignment on next list elements using the first_separator_position
     assert(data.find('|') == first_separator_position)
 
